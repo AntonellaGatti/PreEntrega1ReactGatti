a9138734d693434f41457a134f27290e4b64a4fe
Antonella Gatti <antonellagatti@MacBook-Air-de-Antonella.local>
2023-09-23 15:39:44 -0300

    firebase

--- myHoodie/src/main.jsx
+++ myHoodie/src/main.jsx
@@ -12,12 +12,12 @@ import {initializeApp} from "firebase/app";
 
 
 const firebaseConfig = {
-  apiKey: "AIzaSyD9EBPg2xUOPVlhKM4B6Dd5aaoetOU88QY",
-  authDomain: "myhoodie-dd2de.firebaseapp.com",
-  projectId: "myhoodie-dd2de",
-  storageBucket: "myhoodie-dd2de.appspot.com",
-  messagingSenderId: "614458342101",
-  appId: "1:614458342101:web:a7d320a01393cc207c805c"
+  apiKey: import.meta.env.VITE_FIRESTORE_API_KEY,
+  authDomain: import.meta.env.VITE_FIRESTORE_AUTH_DOMAIN,
+  projectId: import.meta.env.VITE_FIRESTORE_PROJECT_ID,
+  storageBucket: import.meta.env.VITE_FIRESTORE_STORAGE_BUCKET,
+  messagingSenderId: import.meta.env.VITE_FIRESTORE_MESSAGING_SENDER_ID,
+  appId: import.meta.env.VITE_FIRESTORE_APP_ID,
 };
 
 initializeApp(firebaseConfig);
